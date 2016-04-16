e4bdb918b71b9f1f7c575cf067fb5dc643f6f15f
Gunar C. Gessner <gunar@gunargessner.com>
2016-04-16 11:43:19 -0300

    Fix test

--- test/parser.js
+++ test/parser.js
@@ -1,10 +1,11 @@
 const test = require('tape')
 const parser = require('../parser')
+const path = require('path')
 const R = require('ramda')
 
 test('Should parse functions in separate files', t => {
   t.plan(1)
-  parser('mocks/file.js', tuples => {
+  parser('test/mocks/file.js', tuples => {
     const aFnInAnotherFile = '[./anotherFile]aFnInAnotherFile'
     const callToAnother = R.pipe(R.last, R.equals(aFnInAnotherFile))
     const findCallInAnotherFile = R.find(callToAnother)
